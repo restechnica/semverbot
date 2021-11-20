063946b90dcadbea5d935773b6d26d26fd3a5912
shiouen <olivier.va@outlook.com>
2021-11-20 23:49:02 +0100

    added CommandError tests - added NewCommandError function

--- internal/commands/error.go
+++ internal/commands/error.go
@@ -4,12 +4,22 @@ import (
 	"fmt"
 )
 
+var CommandErrorTemplate = "command %s exited with %s, output: \n%s"
+
 type CommandError struct {
 	Arguments []string
-	Output    string
 	Err       error
+	Output    string
+}
+
+func NewCommandError(arguments []string, output string, err error) CommandError {
+	return CommandError{
+		Arguments: arguments,
+		Err:       err,
+		Output:    output,
+	}
 }
 
 func (e CommandError) Error() string {
-	return fmt.Sprintf("command %s exited with %s, output: \n%s", e.Arguments, e.Err.Error(), e.Output)
+	return fmt.Sprintf(CommandErrorTemplate, e.Arguments, e.Err.Error(), e.Output)
 }
