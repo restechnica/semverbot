7bcb88b1d37861e1b47ce0b18287d78419a3f1bc
Olivier Van Aken <olivier.vanaken@nike.com>
2023-06-09 11:06:18 +0200

    moved all current commands into a v1 package - added v1 command as a subcommand as well

---
+++ pkg/cli/commands/v1/root.go
@@ -0,0 +1,23 @@
+package v1
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// NewV1Command creates a new V1 root command.
+// Returns the new V1 root command.
+func NewV1Command() *cobra.Command {
+	var command = &cobra.Command{
+		Use:   "v1",
+		Short: "v1 sbot API",
+	}
+
+	command.AddCommand(NewGetCommand())
+	command.AddCommand(NewInitCommand())
+	command.AddCommand(NewPredictCommand())
+	command.AddCommand(NewPushCommand())
+	command.AddCommand(NewReleaseCommand())
+	command.AddCommand(NewUpdateCommand())
+
+	return command
+}
