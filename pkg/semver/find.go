3f05fe67219482f6e142c3207a1f1f629dacb961
Olivier Van Aken <olivier.va@outlook.com>
2023-09-11 18:02:19 +0200

    reworked the semver.Find function to find the biggest valid semver version, no matter in which order the original versions were passed on

--- pkg/semver/find.go
+++ pkg/semver/find.go
@@ -1,15 +1,41 @@
 package semver
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
 
-// Find finds a valid semver version in a slice of strings.
-// Returns a version when found, otherwise an error stating no valid semver version has been found.
+	blangsemver "github.com/blang/semver/v4"
+)
+
+// Find finds the biggest valid semver version in a slice of strings.
+// The initial order of the versions does not matter.
+// Returns the biggest valid semver version if found, otherwise an error stating no valid semver version has been found.
 func Find(versions []string) (found string, err error) {
+	var parsedVersions blangsemver.Versions
+	var parsedVersion blangsemver.Version
+
 	for _, version := range versions {
-		if _, err = Parse(version); err == nil {
-			return version, err
+		if parsedVersion, err = Parse(version); err != nil {
+			continue
+		}
+
+		parsedVersions = append(parsedVersions, parsedVersion)
+	}
+
+	if len(parsedVersions) == 0 {
+		return found, fmt.Errorf("could not find a valid semver version")
+	}
+
+	blangsemver.Sort(parsedVersions)
+
+	var targetVersion = parsedVersions[len(parsedVersions)-1]
+
+	// necessary because blangsemver's Version.String() strips any prefix
+	for _, version := range versions {
+		if strings.Contains(version, targetVersion.String()) {
+			return version, nil
 		}
 	}
 
-	return found, fmt.Errorf("could not find a valid semver version")
+	return found, nil
 }
