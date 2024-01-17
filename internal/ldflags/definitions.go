c7b0c8c7e086a48b6bc4abfc5fb3d2923d0d8fb3
Olivier Van Aken <olivier.va@outlook.com>
2024-01-17 09:52:26 +0100

    added some docs to ldflags package

--- internal/ldflags/definitions.go
+++ internal/ldflags/definitions.go
@@ -1,6 +1,7 @@
 package ldflags
 
+// -X key=value ldflags definitions, injected at build time.
 var (
-	// Version The version of the semverbot binary, injected at build time.
+	// Version The release version of the sbot binary.
 	Version string = "dev"
 )
