a4f6ff453078027d8118ff68754c530ddf613850
Olivier Van Aken <olivier.va@outlook.com>
2024-01-17 12:58:14 +0100

    added .semverbot/config.toml to default additional config filepaths

--- pkg/cli/defaults.go
+++ pkg/cli/defaults.go
@@ -8,7 +8,7 @@ import (
 
 var (
 	// DefaultAdditionalConfigFilePaths additional default relative filepaths to the config file.
-	DefaultAdditionalConfigFilePaths = []string{".sbot.toml", ".sbot/config.toml"}
+	DefaultAdditionalConfigFilePaths = []string{".sbot.toml", ".semverbot/config.toml", ".sbot/config.toml"}
 
 	// DefaultConfigFilePath the default relative filepath to the config file.
 	DefaultConfigFilePath = internal.DefaultConfigFilePath
